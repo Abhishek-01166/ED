a963393e2dda6795d5e3928ece296c65823ac76c
Abhishek-01166 <Abhishek.gniit300@gmail.com>
2026-04-24 18:55:48 +0530

    Refactor startup architecture and enable DbContext pooling

--- ED/ED/Program.cs
+++ ED/ED/Program.cs
@@ -1,45 +1,16 @@
-using ED.Data;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.EntityFrameworkCore;
+using ED.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services
-    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Account/Login";
-        options.AccessDeniedPath = "/Account/Login";
-    });
-
-builder.Services.AddAuthorization();
+    .AddPresentation()
+    .AddDataAccess(builder.Configuration)
+    .AddCookieAuth();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    await DbInitializer.SeedAsync(dbContext);
-}
+app.UseConfiguredMiddleware();
+await app.SeedDatabaseAsync();
 
 app.MapControllerRoute(
     name: "default",
